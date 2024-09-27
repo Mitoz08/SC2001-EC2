334a350eb795938d9a176b2d30aee9a9f2a75c95
Mitoz08 <timong22@yahoo.com>
2024-09-27 16:33:20 +0800

    Created AdjacencyList

--- src/GraphCreation/GraphGenerator.java
+++ src/GraphCreation/GraphGenerator.java
@@ -4,6 +4,24 @@ import GraphCreation.AdjacencyList.AdjacencyList;
 import GraphCreation.AdjacencyMatrix.AdjacencyMatrix;
 
 public class GraphGenerator {
+
+
+    public static void Generate(int Vertices, AdjacencyList AdjList, AdjacencyMatrix AdjMatrix) {
+        // Add Class Object to the parameter to run addEdge Function
+
+        for (int i = 0; i < Vertices; i++) {
+            for (int j = i + 1; j < Vertices; j++) {
+                // Create GraphCreation.Edge
+
+                Edge newEdge = new Edge(i, j, randomWeight()); // Generate random weight
+
+                // Run addEdge function on both class
+                AdjList.addEdge(newEdge);
+                AdjMatrix.addEdge(newEdge);
+            }
+        }
+    }
+
     public static void Generate(int Vertices, int Edges, AdjacencyList AdjList, AdjacencyMatrix AdjMatrix) {
         // Add Class Object to the parameter to run addEdge Function
 
@@ -17,7 +35,7 @@ public class GraphGenerator {
                 if (Math.random() < probability){
                     // Create GraphCreation.Edge
 
-                    Edge newEdge = new Edge(i,j,10); // Generate random weight
+                    Edge newEdge = new Edge(i,j,randomWeight()); // Generate random weight
 
                     // Run addEdge function on both class
                     AdjList.addEdge(newEdge);
@@ -52,6 +70,9 @@ public class GraphGenerator {
 
             }
         }
+    }
 
+    private static int randomWeight(){
+        return 1 + (int)(Math.random()*999);
     }
 }
