4dc4c9f74b7bb23d52c5be5b764494afb7e6698d
julianodetto <julianodetto@gmail.com>
2024-09-29 17:02:47 +0800

    Added logic in main to export to a csvFile

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,32 @@ import GraphCreation.GraphGenerator;
 import AdjacencyList.AdjacencyList;
 import Dijkstra.Dijkstra;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        for (int maxVertex=5; maxVertex<=5; maxVertex+=5) {
-            for (int repeat = 1; repeat <= 20; repeat++) {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File csvFile = new File("Dijkstra.csv");
+        PrintWriter out = new PrintWriter(csvFile);
+
+        out.println("maxVertex, repeat, timeTaken");
+
+        for (int maxVertex=5; maxVertex<=10; maxVertex+=5) {
+            for (int repeat = 1; repeat <= 10; repeat++) {
                 AdjacencyList list = new AdjacencyList(maxVertex);
                 AdjacencyMatrix matrix = new AdjacencyMatrix(maxVertex);
                 GraphGenerator.Generate(maxVertex, 2*maxVertex, list, matrix);
                 Dijkstra test = new Dijkstra();
-                int[] a1 = test.Dijkstra(list,0);
-                int[] a2 = test.Dijkstra(matrix,0);
-                System.out.println(Arrays.toString(a1));
-                System.out.println(Arrays.toString(a2));
-
+                long startTime = System.nanoTime();
+                test.Dijkstra(list,0);
+                long endTime = System.nanoTime();
+                long timeTaken = (endTime-startTime)/1000; //in microseconds
+                out.printf("%d, %d, %d\n", maxVertex, repeat, timeTaken);
             }
         }
+        out.close();
     }
 }
