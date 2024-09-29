116a2185d038a45ae074d329296c520025d41534
julianodetto <julianodetto@gmail.com>
2024-09-29 13:24:15 +0800

    Added Dijkstra algo with matrix input. Imported ArrayList and used a sorted list approach (insertion sort)

--- src/Dijkstra/Dijkstra.java
+++ src/Dijkstra/Dijkstra.java
@@ -4,6 +4,8 @@ import AdjacencyList.AdjacencyList;
 import AdjacencyList.LinkedList;
 import AdjacencyList.ListNode;
 import AdjacencyMatrix.AdjacencyMatrix;
+
+import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 
@@ -13,7 +15,7 @@ public class Dijkstra {
         int[] dis = new int[list.getMaxVertex()];
         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.weight - b.weight);
         for (int i=0; i<list.getMaxVertex(); i++){
-            dis[i] = 1001;
+            dis[i] = Integer.MAX_VALUE; //Assign MAX INTEGER
         }
         dis[source] = 0;
         pq.add(new Pair(source,0));
@@ -23,18 +25,18 @@ public class Dijkstra {
             int weight = curPair.weight;
 
             LinkedList temp = new LinkedList();
-            temp = list.getAdjacencyList(curNode);
+            temp = list.getAdjacencyList(curNode); //Finding initial vertex
 
             ListNode node = new ListNode();
             node = temp.getHead();
-            while (node != null){
+            while (node != null){ //Stop until there is no more neighbours
                 int neighbor = node.tVertex;
                 int curWeight = node.weight;
                 if (weight + curWeight < dis[neighbor]){
                     dis[neighbor] = weight + curWeight;
                     pq.add(new Pair(neighbor, dis[neighbor]));
                 }
-                node = node.nextNode;
+                node = node.nextNode; //Finding the next neighbour
             }
 
         }
@@ -43,11 +45,34 @@ public class Dijkstra {
     public int[] Dijkstra(AdjacencyMatrix matrix, int source){
         int[] dis = new int[matrix.getMaxVertex()];
         for (int i=0; i< matrix.getMaxVertex(); i++){
-            dis[i] = 1001;
+            dis[i] = Integer.MAX_VALUE; //Assign MAX INTEGER
         }
-        dis[source] = 0;
+        dis[source] = 0; //INITIALISE distance of source vertex
+        ArrayList<Pair> pq = new ArrayList<>(); //Using a sorted list to add a 2-tuple (initial vertex, Weight of edge)
+        pq.add(new Pair(source, 0)); //"Enqueue" into a priority queue
+        while (!pq.isEmpty()){
+            Pair curPair = pq.remove(0); //remove the element from the top of the priority queue
+            int curNode = curPair.node;
+            int weight  = curPair.weight;
 
+            for (int l=0; l< matrix.getMaxVertex(); l++){
+                int curWeight = matrix.getWeight(curNode,l);
+                if (weight + curWeight < dis[l] && curWeight != -1){
+                    dis[l] = weight + curWeight;
+                    add(pq, new Pair(l,dis[l])); //"Enqueue" into a priority queue while by inserting it in ascending order
+                }
+            }
 
+        }
         return dis;
+    }
+    private void add(ArrayList<Pair> pq, Pair newPair) {
+        int i = 0;
+        // Find the correct position to insert the new pair
+        while (i < pq.size() && pq.get(i).weight < newPair.weight) {
+            i++;
+        }
+        // Insert the new pair at the correct position
+        pq.add(i, newPair); //basic inserting of element into an array, inserts the new element and shifts everything to the right
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@ import GraphCreation.GraphGenerator;
 import AdjacencyList.AdjacencyList;
 import Dijkstra.Dijkstra;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i=1; i<=5; i++) {
-            AdjacencyList list = new AdjacencyList(5);
-            AdjacencyMatrix matrix = new AdjacencyMatrix(5);
-            GraphGenerator.Generate(5, 7, list, matrix);
-            Dijkstra test = new Dijkstra();
-            int[] array = new int[5];
-            long startTime = System.nanoTime();
-            array = test.Dijkstra(list, 0);
-            long endTime = System.nanoTime();
-            long timeTaken = (endTime - startTime) / 1000;
-            matrix.print();
-            System.out.println("The time taken for Dijkstra is " + timeTaken + " microseconds");
+        for (int maxVertex=5; maxVertex<=5; maxVertex+=5) {
+            for (int repeat = 1; repeat <= 20; repeat++) {
+                AdjacencyList list = new AdjacencyList(maxVertex);
+                AdjacencyMatrix matrix = new AdjacencyMatrix(maxVertex);
+                GraphGenerator.Generate(maxVertex, 2*maxVertex, list, matrix);
+                Dijkstra test = new Dijkstra();
+                int[] array = test.Dijkstra(list,0);
+                System.out.println(Arrays.toString(array));
+
+            }
         }
     }
 }
